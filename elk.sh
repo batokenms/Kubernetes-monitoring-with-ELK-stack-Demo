6fb4e33129f8b8674970dcb2fca2587359932a50
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-08-09 08:08:47 -0400

    Create elk.sh

---
+++ elk.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# elasticsearch-ss Deployment 
+
+kubectl apply -f elasticsearch-ss.yaml -n josh 
+
+# logstash-deployment Deployment 
+
+kubectl apply -f logstash-deployment.yaml -n josh
+
+# filebeat-ds Deployment
+
+kubectl apply -f filebeat-ds.yaml -n josh
+
+# metricbeat-ds Deployment 
+
+kubectl apply -f metricbeat-ds.yaml -n josh
+
+# kibana-deployment Deployment
+
+kubectl apply -f kibana-deployment.yaml -n josh
+
+# Deployment curator-cronjob
+
+kubectl apply -f curator-cronjob.yaml -n josh
